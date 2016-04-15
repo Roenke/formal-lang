ef8276d7e102144a1d3de4783687291667ee3be3
Roenke <roenke54@gmail.com>
2016-04-15 22:44:31 +0300

    Small fixes for parser.

--- ha6/parser/parser/Parser/LLanguageParser.cs
+++ ha6/parser/parser/Parser/LLanguageParser.cs
@@ -28,93 +28,15 @@ namespace parser.Parser
         private static Parser<Term> _variable;
         private static Parser<Term> _expr;
 
-        private static Parser<Program> _program; 
+        private static Parser<Program> _program;
+
+        public LLanguageParser()
+        {
+            Init();
+        }
 
         public Program GetTree(string programToParse)
         {
-            Parser<Term>[] exprlazy = {null};
-            _expr = Prim.Lazy(() => exprlazy[0]);
-            Func<Parser<Term>, Parser<Term>> @try = Prim.Try;
-
-            var def = new Language();
-            var lexer = Tok.MakeTokenParser<Term>(def);
-            var binops = BuildOperatorsTable(lexer);
-
-            var parens = lexer.Parens;
-            var reserved = lexer.Reserved;
-            var reservedOp = lexer.ReservedOp;
-
-            _number = from n in lexer.Integer
-                      select new Number(n) as Term;
-
-            _variable = from id in lexer.Identifier
-                        select new Variable(id) as Term;
-
-            _read =
-                from r in reserved("read")
-                from e in _expr
-                select new OperationIo(IoOperationType.Read, e as Expression, r.Location) as Statement;
-
-            _write =
-                from w in reserved("write")
-                from e in _expr
-                select new OperationIo(IoOperationType.Write, e as Expression, w.Location) as Statement;
-
-            _skip = from s in reserved("skip")
-                    select new SkipStatement(s.Location) as Statement;
-
-            _assign =
-                from v in _variable
-                from op in reservedOp(":=")
-                from e in _expr
-                select new AssignStatement(v as Variable, e as Expression, v.Location) as Statement;
-
-            _whileDo =
-                from _ in reserved("while")
-                from e in _expr
-                from __ in reserved("do")
-                from s in _termStatement
-                select new WhileDoStatement(e, s) as Statement;
-
-            _ifThenElse =
-                from i in reserved("if")
-                from c in _expr
-                from then in reserved("then")
-                from st1 in _termStatement
-                from else_ in reserved("else")
-                from st2 in _termStatement
-                select new IfStatement(c as Expression, st1, st2, i.Location) as Statement;
-
-            _termStatement =
-                from s in _skip | _assign | _read | _write | _whileDo | _ifThenElse
-                select s;
-
-            _semiStatement =
-                from t in _termStatement
-                from _ in reservedOp(";")
-                from st in _semiStatement
-                select new SemiStatement(t, st) as Statement;
-
-            _statement =
-                from s in _semiStatement | _termStatement
-                select s;
-
-            var subexpr =
-                from p in parens(_expr)
-                select p;
-
-            var factor = 
-                from f in @try(_number)
-                                   | @try(_variable)
-                                   | subexpr
-                select f;
-
-            _program = 
-                from s in _statement
-                select new Program(s);
-
-            exprlazy[0] = Ex.BuildExpressionParser(binops, factor);
-
             var result = _program.Parse(programToParse);
 
             if (!result.IsFaulted && result.Value.Length > 1)
@@ -162,6 +84,94 @@ namespace parser.Parser
                 .AddRow().Add(lor);
 
             return binops;
+        }
+
+        private static void Init()
+        {
+            // TODO: Impelent custom Expression parser.
+
+            Parser<Term>[] exprlazy = { null };
+            _expr = Prim.Lazy(() => exprlazy[0]);
+            Func<Parser<Term>, Parser<Term>> @try = Prim.Try;
+
+            var def = new Language();
+            var lexer = Tok.MakeTokenParser<Term>(def);
+            var binops = BuildOperatorsTable(lexer);
+
+            var parens = lexer.Parens;
+            var reserved = lexer.Reserved;
+            var reservedOp = lexer.ReservedOp;
+
+            _number = from n in lexer.Integer
+                      select new Number(n) as Term;
+
+            _variable = from id in lexer.Identifier
+                        select new Variable(id) as Term;
+
+            _read =
+                from r in reserved("read")
+                from e in _expr
+                select new OperationIo(IoOperationType.Read, e as Expression, r.Location) as Statement;
+
+            _write =
+                from w in reserved("write")
+                from e in _expr
+                select new OperationIo(IoOperationType.Write, e as Expression, w.Location) as Statement;
+
+            _skip = from s in reserved("skip")
+                    select new SkipStatement(s.Location) as Statement;
+
+            _assign =
+                from v in _variable
+                from op in reservedOp(":=")
+                from e in _expr
+                select new AssignStatement(v as Variable, e as Expression, v.Location) as Statement;
+
+            _whileDo =
+                from w in reserved("while")
+                from e in _expr
+                from __ in reserved("do")
+                from s in _termStatement
+                select new WhileDoStatement(e as Expression, s, w.Location) as Statement;
+
+            _ifThenElse =
+                from i in reserved("if")
+                from c in _expr
+                from then in reserved("then")
+                from st1 in _termStatement
+                from else_ in reserved("else")
+                from st2 in _termStatement
+                select new IfStatement(c as Expression, st1, st2, i.Location) as Statement;
+
+            _termStatement =
+                from s in _skip | _assign | _read | _write | _whileDo | _ifThenElse
+                select s;
+
+            _semiStatement =
+                from t in _termStatement
+                from _ in reservedOp(";")
+                from st in _semiStatement
+                select new SemiStatement(t, st) as Statement;
+
+            _statement =
+                from s in _semiStatement | _termStatement
+                select s;
+
+            var subexpr =
+                from p in parens(_expr)
+                select p;
+
+            var factor =
+                from f in @try(_number)
+                                   | @try(_variable)
+                                   | subexpr
+                select f;
+
+            _program =
+                from s in _statement
+                select new Program(s);
+
+            exprlazy[0] = Ex.BuildExpressionParser(binops, factor);
         }
     }
 }
